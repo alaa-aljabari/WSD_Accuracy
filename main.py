a8058e54e5cbaabae1d1ed7aa1403bb5c29f2888
alaa-aljabari <155227590+alaa-aljabari@users.noreply.github.com>
2024-03-08 23:17:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -197,32 +197,31 @@ def WSD(sentence, targetWord):
         return outputList
 
 
-def WSD_test(sentences_json_path, output_json_path): 
+def my_WSD_test(sentences_json_path, output_json_path):
     outputList = []  
-
     with open(sentences_json_path, 'r', encoding='utf-8') as file:
         sentences = json.load(file)
-        
-    for sentence in sentences:
-        wordsJson = []
-        for word in sentence['words']:
-            conceptId, gloss = WSDdisambiguation(sentence['sentence'], word)
-            wordsJson.append({
-                "word_id": word['word_id'],
-                "word": word['word'],
-                "target_gloss": conceptId  # Assuming only one sense for simplicity
-            })
-        
-        sentenceJson = {
-            "sentence_id": sentence['sentence_id'],
-            "sentence": sentence['sentence'],
-            "words": wordsJson
-        }
-        outputList.append(sentenceJson)
-        
-    with open(output_json_path, 'w', encoding='utf-8') as outfile:
-       json.dump(outputList, outfile)#, ensure_ascii=False, indent=4)
-    
+        for sentence in sentences:
+            wordsJson = []
+            for word in sentence['words']:
+                conceptId, gloss = WSDdisambiguation(sentence['sentence'], word)
+                wordsJson.append({
+                    "word_id": word['word_id'],
+                    "word": word['word'],
+                    "target_gloss": conceptId  # Assuming only one sense for simplicity
+                })
+            
+            sentenceJson = {
+                "sentence_id": sentence['sentence_id'],
+                "sentence": sentence['sentence'],
+                "words": wordsJson
+            }
+            outputList.append(sentenceJson)
+            
+        with open(output_json_path, 'w', encoding='utf-8') as outfile:
+            json.dump(outputList, outfile,ensure_ascii=False, indent=4)
+
+        print("Output JSON file saved at:", output_json_path)
     return outputList
 
 
