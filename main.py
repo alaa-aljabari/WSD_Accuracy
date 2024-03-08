86320ec9bea4d3e91fec2b4cc555a8991656253a
alaa-aljabari <155227590+alaa-aljabari@users.noreply.github.com>
2024-03-08 22:12:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -220,8 +220,10 @@ def WSD_test(sentences_json_path, output_json_path):
         }
         outputList.append(sentenceJson)
     
+    # Moved file writing outside the loop
     with open(output_json_path, 'w', encoding='utf-8') as outfile:
-        json.dump(outputList, outfile, ensure_ascii=False, indent=4)
+        outfile.write(json.dump(outputList, outfile, ensure_ascii=False, indent=4))
+        #json.dump(outputList, outfile, ensure_ascii=False, indent=4)
     
     return outputList
 
