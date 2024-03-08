ae5fa3cad316aaaca5575b7d47fbbf379381d110
alaa-aljabari <155227590+alaa-aljabari@users.noreply.github.com>
2024-03-08 21:50:52 +0200

    Update main.py

--- main.py
+++ main.py
@@ -214,6 +214,7 @@ def WSD_test(sentences_json_path,output_json_path):
                 })
         
         sentenceJson = {"sentence_id": sentence['sentence_id'], "sentence": sentence['sentence'], "words": wordsJson}
+        print(sentenceJson)
         outputList.append(sentenceJson)
     
     with open(output_json_path, 'w', encoding='utf-8') as outfile:
