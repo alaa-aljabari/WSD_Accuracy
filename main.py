00e7b00b9dbf0fb5f6a6508f34de3b0a8572f0e7
alaa-aljabari <155227590+alaa-aljabari@users.noreply.github.com>
2024-03-08 21:53:06 +0200

    Update main.py

--- main.py
+++ main.py
@@ -200,6 +200,7 @@ def WSD(sentence, targetWord):
 def WSD_test(sentences_json_path,output_json_path): 
     outputList = []  
 
+    print ("Helooooooooooooooo")
     with open(sentences_json_path, 'r', encoding='utf-8') as file:
         sentences = json.load(file)
         
@@ -212,7 +213,7 @@ def WSD_test(sentences_json_path,output_json_path):
                 "word": word['word'],
                 "target_gloss": conceptId  # Assuming only one sense for simplicity
                 })
-        
+            
         sentenceJson = {"sentence_id": sentence['sentence_id'], "sentence": sentence['sentence'], "words": wordsJson}
         print(sentenceJson)
         outputList.append(sentenceJson)
